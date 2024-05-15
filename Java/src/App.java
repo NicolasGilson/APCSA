273c651a762702317b9ff9ccc828ee9869987f61
NicolasGilson <87547781+NicolasGilson@users.noreply.github.com>
2024-05-15 11:35:17 -0400

    fix

--- Java/src/App.java
+++ Java/src/App.java
@@ -3,6 +3,9 @@ import MayWeekOne.cTOf;
 public class App {
     public static void main(String[] args) throws Exception 
     {
+        System.out.println(cTOf.convertTemp(0));
+        System.out.println(cTOf.convertTemp(2));
+        System.out.println(cTOf.convertTemp(98));
         System.out.println(cTOf.convertTemp(98));
     }
 }
