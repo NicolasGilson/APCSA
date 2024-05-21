e5272895ffeeb0ea3697c2d55b2dfef194016b6e
NicolasGilson <87547781+NicolasGilson@users.noreply.github.com>
2024-05-21 11:01:59 -0400

    yesterday work

--- Java/src/App.java
+++ Java/src/App.java
@@ -5,6 +5,6 @@ import apple.MayWeekOne.randMatrix;
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println(passChecker.checker("12345678912"));
+        System.out.println(passChecker.checker("abc123abc3"));
     }
 }
--- Java/src/apple/MayWeekOne/passChecker.java
+++ Java/src/apple/MayWeekOne/passChecker.java
@@ -4,7 +4,7 @@ public class passChecker
 {
     public static boolean checker(String pass)
     {
-        if(pass.length()>=10&&pass.matches("[a-zA-Z0-9]+")&&pass.parseInt)
+        if(pass.length()>=10&&pass.matches("[a-zA-Z0-9]+")&& pass.matches(".*\\d.*\\d.*\\d.*"))
         {
             return true;
         }
