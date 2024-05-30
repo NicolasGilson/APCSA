8eb479411a90fb32f2e70176a25c59d49a467c23
NicolasGilson <87547781+NicolasGilson@users.noreply.github.com>
2024-05-30 11:43:42 -0400

    TTT
    TTT done

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,10 +1,26 @@
-import apple.MayWeekOne.passChecker;
-import apple.MayWeekOne.randMatrix;
+import java.util.Scanner;
+
+import apple.MayWeekOne.TTT;
 
 
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println(passChecker.checker("abc123abc3"));
+        Scanner scann = new Scanner(System.in);
+        System.out.print("What is the first player's name: ");
+        String p1 = scann.nextLine();
+        System.out.print("What is the second player's name: ");
+        String p2 = scann.nextLine();
+        TTT gameOne = new TTT(p1,p2);
+        System.out.println("This is a 3x3 board, you may place your move in any coordinate ranging 0, 1, and 2.");
+        while(gameOne.check() != true)
+        {
+            System.out.print("What row would you like to place it in (0, 1, 2): ");
+            int row = scann.nextInt();
+            System.out.print("What column would you like to place it in (0, 1, 2): ");
+            int column = scann.nextInt();
+            gameOne.UserTurn(row, column);
+        }
+        scann.close();
     }
 }
---
+++ Java/src/apple/MayWeekOne/TTT.java
@@ -0,0 +1,81 @@
+package apple.MayWeekOne;
+
+public class TTT 
+{
+    public String p1;
+    public String p2;
+    char[][] board = new char[3][3];
+    public int turn = 1;
+    
+//run the parameters 
+    public TTT(String p1, String p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        for(int i=0; i< 3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                board[i][j] = '-';
+            }
+        }
+        for(int p = 0; p < 3; p++)
+        {
+            for(int q = 0; q < 3; q++)
+            {
+                System.out.print(board[p][q]);
+            }
+            System.out.println();
+        }
+    }
+    public void UserTurn(int row1, int column1)
+    {
+        if(board[row1][column1] == (char)(67)||board[row1][column1] == (char)(65))
+        {
+            System.out.println("That turn is not valid");
+        }
+        else
+        {
+            board[row1][column1] = (char)(turn+66);
+            turn*=-1;
+            for(int p = 0; p < 3; p++)
+            {
+                for(int q = 0; q < 3; q++)
+                {
+                    System.out.print(board[p][q]);
+                }
+                System.out.println();
+            }
+        }
+    }
+    public boolean check()
+    {
+        String w = p2;
+        if(turn == 1)
+        {
+            w=p1;
+        }
+        for(int i = 0; i <board.length;i++)
+        {
+            if(board[i][0]==board[i][1]&&board[i][0]==board[i][2]&&board[i][0]!='-')
+            {
+                System.out.println(w);
+                return true;
+            }
+            if(board[0][i]==board[1][i]&&board[0][i]==board[2][i]&&board[0][i]!='-')
+            {
+                return true;
+            }
+        }
+        if(board[0][2] == board[1][1] && board[0][2] == board[2][0]&&board[0][2]!='-')
+        {
+            return true;
+        }
+        if(board[0][0] == board[1][1] && board[0][2] == board[2][2]&&board[0][0]!='-')
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
